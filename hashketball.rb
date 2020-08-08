618311a70095174defcaf60515c8439e1bf5c56d
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-08 22:51:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [ {
-          :
+          :player_name => ""
           :number => 0,
           :shoe => 16,
           :points => 22,
